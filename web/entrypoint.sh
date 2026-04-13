d4a6905f2cec5d7eafb1a6480b4958abb3daa146
Arvind <pingarvindforquestions@gmail.com>
2026-04-13 23:05:43 +0530

    feat(*): removed unused files

--- web/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/opt/venv/bin/python manage.py collectstatic --noinput
-
-/opt/venv/bin/python manage.py makemigrations
-/opt/venv/bin/python manage.py migrate --noinput
-
-APP_PORT=${PORT:-8000}
-/opt/venv/bin/daphne backend.asgi:application --bind "0.0.0.0" --port ${APP_PORT}
